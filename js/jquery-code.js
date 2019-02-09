9dfd5f4d2151f21a14e57891124b85dba279d264
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-09 16:38:56 -0500

    [$] Valida el formulario usando jQuery

--- js/jquery-code.js
+++ js/jquery-code.js
@@ -1,29 +1,29 @@
 $( document ) .ready( function() {
 
-  // Deshabilita el botón "Continuar"
-  $( '#btn_continue' ) .attr( 'disabled', 'disabled' );
-
-  // Limpia los campos
-  $( 'form :input' ) .val( '' );
-
-  //Alterna Habilita Continua
-  function toggleEnableContinue() {
-
-    // Valida que los campos no estén vacíos
-    if( $( '#first_name' ) .val() != '' && $( '#last_name' ) .val() != '' ) {
-      $( '#btn_continue' ) .removeAttr( 'disabled' );         // Habilita el botón "Continuar"
-    }
-    else {
-      $( '#btn_continue' ) .attr( 'disabled', 'disabled' );   // Deshabilita el botón "Continuar"
-    }
-
+  function restartForm() {
+    $( '#btn_continue' ) .attr( 'disabled', 'disabled' );   // Deshabilita el botón "Continuar"
+    $( 'form :input' ) .val( '' );                          // Limpia los campos
   }
+
+  function isFormValid() {
+    return $( '#first_name' ) .val() != '' && $( '#last_name' ) .val() != '';
+  }
+
+  //Alterna acción de Habilitar/Deshabilitar Botón "Continuar"
+  function toggleEnableContinue() {
+    // Valida que los campos no estén vacíos
+    $( '#btn_continue' ) .prop( 'disabled', !isFormValid() );
+  }
+
+  restartForm();
 
   // Lee la interacción con el teclado
   $( '#first_name, #last_name' ) .keyup( toggleEnableContinue );
 
   $( '#form_register' ) .submit( function( event ) {
     event .preventDefault();           // Evita la recarga al envio de los datos
+
+    if( !isFormValid() ) return;        // Detiene la ejecución si el formulario no es válido
 
     $( '#register, .card-title, .card-subtitle' ) .hide();
     $( '#welcome' ) .show();
