76a4fc491afa46b7187dc571025b595b5c5630e1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-09 17:44:50 -0500

    [+] Oculta Formulario, muestra Bienvenida al pulsar botón 'continuar'

--- js/vue-code.js
+++ js/vue-code.js
@@ -4,11 +4,16 @@ new Vue({
   data: {         // Inicializamos los valores (inputs)
     // Se enlazan al DOM usando la directiva v-model (agregando comportamiento reactivo al DOM)
     first_name: '',
-    last_name: ''
+    last_name: '',
+    formSubmited: false
   },
   methods: {
     isFormValid: function() {
       return this .first_name != '' && this .last_name != '';
+    },
+    submitForm: function() {          // Se enlaza esta acción al formulario usando la directiva v-on
+      if( ! this .isFormValid() ) return;    // Si el formulario no es válido detiene la ejecución
+      this .formSubmited = true;
     }
   }
 });
