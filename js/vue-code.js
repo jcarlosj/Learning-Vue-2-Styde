3fbed91c3e22ac9f28b59ef7f78c5136d64ba3d1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-10 13:29:59 -0500

    [+] Usa métodos JavaScript en propiedades interpoladas
    Aclaración:
    Es importante tener en cuenta varias cosas dentro de las
    interpolaciones:
    
      1. Escribir una sola sentencia por interpolación
      2. Las interpolaciones pueden realizar operaciones
         matemáticas.
      3. Se pueden aplicar métodos de JavaScript a las propiedades
         dentro de una interpolación.
      4. Se puede llamar el nombre de la propiedad o método usando
         solo su nombre o anteponiendo la palabra reservada this
         concatenandola al nombre de la propiedad con un PUNTO

--- js/vue-code.js
+++ js/vue-code.js
@@ -1,20 +1,10 @@
-// Nueva instancia de Vue
+// Code Vue
 let vm = new Vue({
   el: '#app',     // Elemento donde funcionará nuestra Aplicación
   data: {         // Inicializamos los valores (inputs)
     // Se enlazan al DOM usando la directiva v-model (agregando comportamiento reactivo al DOM)
     first_name: '',
-    last_name: '',
-    formSubmited: false
-  },
-  methods: {
-    isFormValid: function() {
-      return this .first_name != '' && this .last_name != '';
-    },
-    submitForm: function() {          // Se enlaza esta acción al formulario usando la directiva v-on
-      if( ! this .isFormValid() ) return;    // Si el formulario no es válido detiene la ejecución
-      this .formSubmited = true;
-    }
+    last_name: ''
   }
 });
 
