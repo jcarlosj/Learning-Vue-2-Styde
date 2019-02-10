4f9c9eb84a7cd1d0949cb157299cfc84edd4ba1c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-10 11:16:50 -0500

    [+] Asignar instancia Vue a una variable, modificar propiedades
    Recomendaciones:
     1. Usar el plugin 'Vue DevTools' en Firefox o Chrome
     2. Siempre definir los datos (atributos) de la aplicación (vacíos si
        es el caso)
     3. Al seleccionar el elemento o tag HTML sobre el que correrá la
        aplicación Vue, NO usar el nombre del elemento como se hacía en Vue 1.
        Se recomienda hacer referencia al id o class del mismo.
     4. Si no se asigna la instancia a una variable se puede hacer
        referencia a esta usando $vm0 y concatenando las propiedades o métodos
        que posee usando el . PUNTO, de otra manera se puede usar el nombre de
        la variable a la que hemos asignado sin el símbolo pesos.
    
    Aclaración:
    Las modificaciones usadas en este ejemplo son exclusivamente para
    manipular propiedades desde la consola de desarrollo del navegador

--- js/vue-code.js
+++ js/vue-code.js
@@ -1,5 +1,5 @@
 // Nueva instancia de Vue
-new Vue({
+let vm = new Vue({
   el: '#app',     // Elemento donde funcionará nuestra Aplicación
   data: {         // Inicializamos los valores (inputs)
     // Se enlazan al DOM usando la directiva v-model (agregando comportamiento reactivo al DOM)
@@ -17,3 +17,7 @@ new Vue({
     }
   }
 });
+
+// Asignamos un nombre por defecto
+vm .first_name = "Elisa";
+vm .last_name = "Giraldo";
