166334f4a08114326de33eacda84ee76a7d87c58
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-12 09:29:34 -0500

    [+] Condiciona aparición campo experiencia usando 'v-model', 'v-bind', 'v-if'
    Aclaración:
    Cada una de las directivas implementadas en este paso tienen una funcionalidad
    o propósito para que el campo experiencia se despliegue o no de acuerdo a si
    la selección al cuestionamiento ¿Has trabajado con Vue? es si o no.
    
     - La directiva 'v-model' asocia el elemento del DOM con la propiedad en la
       instancia de Vue para esta zona de la aplicación. En nuestro caso a la
       propiedad 'experience'.
     - La directiva 'v-bind' encadenada con : (dos puntos) permite enlazar el
       elemento a una expresión de JavaScript, estas expresiones puedens ser:
       una variable, una propiedad o valores booleanos.
     - La directiva 'v-if' asocia una condición a un elemento que podrá o no
       desplegarse según esta condición se cumpla o no.
    
    NOTA: La expresión v-bind:value="true" puede usarse la sintaxis corta de la
          siguiente forma :value="true" en la que precindimos de la cadena v-bind
          pero mantenemos los : (dos puntos obteniendo los mismos resultados)

--- js/vue-code.js
+++ js/vue-code.js
@@ -1,1 +1,11 @@
 // Code Vue
+let vm = new Vue({
+    el: '#app',
+    data: {
+        vue: {
+            experience: null,
+            years: 0,
+            description: ''
+        }
+    }
+});
