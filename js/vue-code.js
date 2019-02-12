c2f1992795f61ca6da21f5a674bf3ab263ab4e3b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-12 13:05:17 -0500

    [$] Condiciona mensajes informativos (validaciones) usando 'computed properties'

--- js/vue-code.js
+++ js/vue-code.js
@@ -7,5 +7,24 @@ let vm = new Vue({
             years: 0,
             description: ''
         }
+    },
+    computed: {
+        descriptionError: function() {
+            let value = this .vue .description .trim();     // Obtiene el valor del campo
+
+            if( value == '' ) {
+                return 'El campo es obligatorio';
+            }
+            else if( value .length < 15 ) {
+                return 'La descripción debe tener mínimo 15 caracteres';
+            }
+            else if( value .length > 140 ) {
+                return 'La descripción puede tener máximo 140 caracteres';
+            }
+            else {
+                return null;
+            }
+
+        }
     }
 });
