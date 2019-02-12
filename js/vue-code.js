3be48277c6c0d0b983baaebdd265ba78207efc7e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-12 09:00:43 -0500

    [.] Prepara estructura del DOM (para Ejemplo)

--- js/vue-code.js
+++ js/vue-code.js
@@ -1,19 +1,1 @@
 // Code Vue
-let vm = new Vue({
-  el: '#app',     // Elemento donde funcionará nuestra Aplicación
-  data: {         // Inicializamos los valores (inputs)
-    // Se enlazan al DOM usando la directiva v-model (agregando comportamiento reactivo al DOM)
-    first_name: '',
-    last_name: '',
-    nameProject: 'Styde'
-  },
-  computed: {     // Forma de definir propiedades dinámicas en Vue
-    reversedName: function() {
-      return ( this .first_name + ' ' + this .last_name ) .split( '' ) .reverse() .join( '' );
-    }
-  }
-});
-
-// Asignamos un nombre por defecto
-vm .first_name = "<h2>Elisa</h2>";
-vm .last_name = "Giraldo";
