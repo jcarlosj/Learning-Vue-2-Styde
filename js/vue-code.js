544910607b46f7d3643fe7d891fcc59ada126139
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-13 18:49:33 -0500

    [+] Uso de la directiva 'v-for' (iterando propiedades, llaves personalizadas de un objeto Vue)
    Aclaración:
    El orden de las propiedades no esta garantizada en Vue, por lo que
    puede que el orden en un navegador u otro sean totalmente diferentes
    al iterarlos.

--- js/vue-code.js
+++ js/vue-code.js
@@ -3,11 +3,11 @@ let vm = new Vue({
     el: '#app',
     data: {
         user: {
-            firstName: 'Juan Carlos',
-            lastName: 'Jiménez Gutiérrez',
-            userName: 'jcarlosj',
-            github: 'https://www.github.com/jcarlosj',
-            email: 'jcjimenez29@misena.edu.co'
+            'Nombres': 'Juan Carlos',
+            'Apellidos': 'Jiménez Gutiérrez',
+            'Nombre de usuario': 'jcarlosj',
+            'Cuenta Github': 'https://www.github.com/jcarlosj',
+            'Correo Electrónico': 'jcjimenez29@misena.edu.co'
         }
     }
 });
