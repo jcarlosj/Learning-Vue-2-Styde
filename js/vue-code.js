74f0df4d14b2a8865a9d371680ea74956279c8c7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-13 17:20:53 -0500

    [+] Uso de la directiva 'v-for' (iteraciones)
    Aclaración:
    La directiva v-for requiere que primero se haga referencia al indice
    y posteriormente al nombre del Array que se desea recorrer a diferencia
    de PHP donde usando el método foreach se hace al contrario
    
       En vue:
          for( item in array )    o    for( item of array )
    
       En PHP:
          foreach( $array as $item )

--- js/vue-code.js
+++ js/vue-code.js
@@ -2,39 +2,10 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        vue: {
-            experience: null,
-            years: 0,
-            description: ''
-        }
-    },
-    computed: {
-        descriptionError: function() {
-            let value = this .vue .description .trim();     // Obtiene el valor del campo
-
-            if( value == '' ) {
-                return 'El campo es obligatorio';
-            }
-            else if( value .length < 15 ) {
-                return 'La descripción debe tener mínimo 15 caracteres';
-            }
-            else if( value .length > 140 ) {
-                return 'La descripción puede tener máximo 140 caracteres';
-            }
-            else {
-                return null;
-            }
-
-        },
-        descriptionFieldClasses: function() {
-            return [ 'form-group', { 'has-error-description': false }, { 'has-error-description': this .descriptionError } ];
-        },
-        descriptionStyles: function() {
-            if( this .descriptionError ) {
-                return { 'color': 'red', 'font-weight': 'bold' };
-            }
-
-            return {};  // Objeto vacío
-        }
+        tasks: [
+            'Técnicas de Autorización con Laravel',
+            'Métodos de Autentización con Laravel',
+            'Crea una aplicación con Laravel'
+        ]
     }
 });
