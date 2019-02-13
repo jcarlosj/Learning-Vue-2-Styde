a87c7e518628f34a7c9a428f6fa8d36f06747037
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-13 18:38:14 -0500

    [+] Uso de la directiva 'v-for' (iterando propiedades de un objeto Vue)

--- js/vue-code.js
+++ js/vue-code.js
@@ -2,26 +2,12 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        tasks: [
-            {
-                title: 'Técnicas de Autorización con Laravel',
-                pending: false
-            },
-            {
-                title: 'Métodos de Autentización con Laravel',
-                pending: true
-            },
-            {
-                title: 'Crea una aplicación con Laravel',
-                pending: true
-            }
-        ]
-    },
-    computed: {
-        pendingTasks: function() {
-            return this .tasks .filter( function( item ) {
-                return item .pending;
-            });
+        user: {
+            firstName: 'Juan Carlos',
+            lastName: 'Jiménez Gutiérrez',
+            userName: 'jcarlosj',
+            github: 'https://www.github.com/jcarlosj',
+            email: 'jcjimenez29@misena.edu.co'
         }
     }
 });
