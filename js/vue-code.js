173dfd1cbca0a26686499b65e4f35741600a3fdc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-15 11:13:12 -0500

    [+] Filtro personalizado para interpolaciones de texto (Mrs)

--- js/vue-code.js
+++ js/vue-code.js
@@ -2,11 +2,15 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        name: 'Ana María Fernández C'
+        name: 'Ana María Fernández C',
+        gender: 'female'
     },
     filters: {
         upper: function( value ) {
             return value .toUpperCase();
+        },
+        title: function( value ) {
+            return `Mrs. ${ value }`;
         }
     }
 });
