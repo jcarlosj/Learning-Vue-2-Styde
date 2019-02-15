7478471914075ff2e5c164f1a1d3b10a7c793f42
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-15 11:01:07 -0500

    [.] Prepara estructura del DOM y JS (para Ejemplo)

--- js/vue-code.js
+++ js/vue-code.js
@@ -2,47 +2,6 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        new_task: '',
-        tasks: [
-            {
-                title: 'Técnicas de Autorización con Laravel',
-                pending: false
-            },
-            {
-                title: 'Métodos de Autentización con Laravel',
-                pending: true
-            },
-            {
-                title: 'Crea una aplicación con Laravel',
-                pending: true
-            }
-        ]
-    },
-    computed: {
-        pendingTasks: function() {
-            return this .tasks .filter( function( item ) {
-                return item .pending;
-            });
-        }
-    },
-    methods: {
-        createTask: function( pending ) {
-
-            if( this .new_task .trim() .length ) {
-                this .tasks .push({
-                    title: this .new_task,
-                    pending: pending
-                });
-                this .new_task = '';
-            }
-        },
-        changeStatus: function( task ) {
-            task .pending = ! task .pending;
-        },
-        pendingTasks: function() {
-            return this .tasks .filter( function( task ) {
-                return task .pending;
-            });
-        }
+        name: 'Ana María Fernández C'
     }
 });
