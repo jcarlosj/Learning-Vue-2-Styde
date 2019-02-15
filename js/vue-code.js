f16f68b820bae6c9533c3f0a0fc75fad4b0fc02a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-15 11:03:58 -0500

    [+] Filtro personalizado para interpolaciones de texto (convertir a mayúsculas)

--- js/vue-code.js
+++ js/vue-code.js
@@ -3,5 +3,10 @@ let vm = new Vue({
     el: '#app',
     data: {
         name: 'Ana María Fernández C'
+    },
+    filters: {
+        upper: function( value ) {
+            return value .toUpperCase();
+        }
     }
 });
