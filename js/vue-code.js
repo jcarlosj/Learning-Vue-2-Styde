0f2b32aa55d7f147d269a230ef64b3b212c88eb4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-15 13:07:35 -0500

    [+] Implementa librería 'NumeralJS' en Filtro global para interpolaciones de texto

--- js/vue-code.js
+++ js/vue-code.js
@@ -1,13 +1,17 @@
-// Define filtro de forma global
+// Define filtros globales
 Vue .filter( 'capital', function( value ) {
     return value .charAt( 0 ) .toUpperCase() + value .slice( 1 );
+});
+Vue .filter( 'money', function( value ) {
+    return numeral( value ) .format( '$ 0,0.00' );
 });
 // Code Vue
 let vm = new Vue({
     el: '#app',
     data: {
         name: 'Ana María Fernández C',
-        gender: 'female'
+        gender: 'female',
+        salary: 2500
     },
     filters: {
         upper: function( value ) {
